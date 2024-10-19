0265d7b5ba54d1aeeb6c2e75b0acdb443f364eb3
Александр Толдин <ababok7t@gmail.com>
2024-10-20 02:55:12 +0300

    awd

--- tasks/is_prime.py
+++ tasks/is_prime.py
@@ -2,14 +2,5 @@ __all__ = ("is_prime",)
 
 
 def is_prime(number: int) -> bool:
-    """Определяет, является ли число простым.
-
-    Example:
-        >> is_prime(0):
-        False
-        >> is_prime(1):
-        False
-        >> is_prime(4):
-        True
-    """
+    najfnkjaenfjknajfndjaenj
     raise NotImplementedError

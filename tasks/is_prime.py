e9d4f860111334fc84c70fe77f074909d228aff0
Александр Толдин <ababok7t@gmail.com>
2024-10-20 11:47:20 +0300

    update task

--- tasks/is_prime.py
+++ tasks/is_prime.py
@@ -2,5 +2,14 @@ __all__ = ("is_prime",)
 
 
 def is_prime(number: int) -> bool:
-    najfnkjaenfjknajfndjaenj
+    k = 2
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            if i*i == number:
+                k+=1
+            else:
+                k+=2
+    if number > 1 and k == 2:
+        return True
+    return False
     raise NotImplementedError

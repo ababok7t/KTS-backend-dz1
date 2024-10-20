097622e921c0e1c015d3294b2e2942bcc60aca7a
Александр Толдин <ababok7t@gmail.com>
2024-10-20 14:25:31 +0300

    update task

--- tasks/find_shortest_longest_word.py
+++ tasks/find_shortest_longest_word.py
@@ -1,17 +1,11 @@
 __all__ = ("find_shortest_longest_word",)
-
+import re
 
 def find_shortest_longest_word(text: str) -> tuple[str, str] | tuple[None, None]:
-    """Находит самое короткое и самое длинное слово.
-
-    Returns:
-        (<самое короткое слово>, <самое длинное слово>) – если text содержит слова,
-        (None, None) – иначе
-
-    Example:
-        >> find_shortest_longest_word("а бб ввв")
-        ("а", "ввв")
-        >> find_shortest_longest_word(" \n\t ")
-        (None, None)
-    """
+    words = re.findall(r'\w+', text)
+    if not words:  # Если список слов пуст
+        return (None, None)
+    shortest_word = min(words, key=len)  # Ищем самое короткое слово
+    longest_word = max(words, key=len)  # Ищем самое длинное слово
+    return (shortest_word, longest_word)
     raise NotImplementedError

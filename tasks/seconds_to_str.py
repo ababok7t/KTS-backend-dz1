359c2c988bdd2c3d532c39ad881a1e465fa0c79c
Александр Толдин <ababok7t@gmail.com>
2024-10-20 12:50:54 +0300

    update task

--- tasks/seconds_to_str.py
+++ tasks/seconds_to_str.py
@@ -16,4 +16,20 @@ def seconds_to_str(seconds: int) -> str:
         >> seconds_to_str(93600)
         01d02h00m00s
     """
+    day = seconds // 86400
+    seconds = seconds % (24 * 3600)
+    hour = (seconds // 3600) % 24
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+
+    if day == 0:
+        if hour == 0:
+            if minutes == 0:
+                return "%02ds" % (seconds)
+            return "%02dm%02ds" % (minutes, seconds)
+        return "%02dh%02dm%02ds" % (hour, minutes, seconds)
+    return "%02dd%02dh%02dm%02ds" % (day, hour, minutes, seconds)
     raise NotImplementedError
+
+print(seconds_to_str(93600))
